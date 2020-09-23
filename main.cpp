00d107050505553812eb955963dd7e3a8956ef5d
Akira Kawata <akirakawata1@gmail.com>
2020-09-24 00:44:57 +0900

    Save

--- main.cpp
+++ main.cpp
@@ -1,25 +1,43 @@
 #include <GL/glut.h>
+#include <unistd.h>
 
+#include <cstdio>
 #include <cstdlib>
+
+#include "simulation.h"
+
+const double EDGE_LENGTH = 500;
+int time = 0;
 
 // Note that the number of points is kept fairly small because a display
 // callback should NEVER run for too long.
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glPointSize(100.0f);
+    glPointSize(30.0f);
     glBegin(GL_POINTS);
-    glVertex2f(500, 500.0);
-    glVertex2f(0, 0.0);
+
+    usleep(100000);
+    // printf("time = %d =========================\n", time);
+    for (int i = 0; i < N_PARTICLE; i++) {
+        glColor3f(1.0 * (double)i / N_PARTICLE,
+                  1.0 * (double)(N_PARTICLE - i) / N_PARTICLE, 1.0);
+        glVertex2f(get_pos(time, i, 0) * EDGE_LENGTH,
+                   get_pos(time, i, 1) * EDGE_LENGTH);
+        // printf("%lf, %lf\n", get_pos(time, i, 0), get_pos(time, i, 1));
+    }
+
     glEnd();
     glFlush();
+
+    time = (time + 1) % TIME_LENGTH;
 }
 
 // Performs application-specific initialization. Sets colors and sets up a
 // simple orthographic projection.
 void init() {
-    // Set a deep purple background and draw in a greenish yellow.
-    glClearColor(0.25, 0.0, 0.2, 1.0);
+    // Set a black background and draw in a greenish yellow.
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     glColor3f(0.6, 1.0, 0.0);
 
     // Set up the viewing volume: 500 x 500 x 1 window with origin lower left.
@@ -31,12 +49,19 @@ void init() {
 // Initializes GLUT, the display mode, and main window; registers callbacks;
 // does application initialization; enters the main event loop.
 int main(int argc, char** argv) {
+    init_sim();
+    simulate();
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(1000, 1000);
     glutInitWindowPosition(40, 40);
     glutCreateWindow("N body simulation");
     glutDisplayFunc(display);
+    glutIdleFunc(glutPostRedisplay);
     init();
+
     glutMainLoop();
+
+    finish_sim();
 }
--- simulation.cpp
+++ simulation.cpp
@@ -1,38 +1,76 @@
-#include <math.h>
-#include <stdlib.h>
+#include "simulation.h"
 
-const int N_PARTICLE = 100;
-const int TIME_LENGTH = 10000;
-const int DIMENSION = 2;
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const double G = 6.67430e-11;
+const double dt = 1.0;
+const double MAX_MASS = 10000000.0;
 
 double *pos, *vel, *acc, *mas;
 
 double dist(int i, int j) {
-    double dx = pos[i][0] - pos[j][0];
-    double dy = pos[i][1] - pos[j][1];
+    double dx = pos[i * 2 + 0] - pos[j * 2 + 0];
+    double dy = pos[i * 2 + 1] - pos[j * 2 + 1];
     return sqrt(dx * dx + dy * dy);
 }
 
+double get_pos(int time, int index, int xy) {
+    return pos[time * N_PARTICLE * DIMENSION + index * DIMENSION + xy];
+}
+
 void init_sim() {
-    pos = malloc(sizeof(double) * N_PARTICLE * TIME_LENGTH * DIMENSION);
+    pos = (double *)malloc(sizeof(double) * N_PARTICLE * (TIME_LENGTH + 1) *
+                           DIMENSION);
     memset(pos, 0, sizeof(double) * N_PARTICLE * TIME_LENGTH * DIMENSION);
-    vel = malloc(sizeof(double) * N_PARTICLE * DIMENSION);
+    vel = (double *)malloc(sizeof(double) * N_PARTICLE * DIMENSION);
     memset(vel, 0, sizeof(double) * N_PARTICLE * DIMENSION);
-    acc = malloc(sizeof(double) * N_PARTICLE * DIMENSION);
+    acc = (double *)malloc(sizeof(double) * N_PARTICLE * DIMENSION);
     memset(acc, 0, sizeof(double) * N_PARTICLE * DIMENSION);
-    mas = malloc(sizeof(double) * N_PARTICLE * DIMENSION);
-    memset(mas, 0, sizeof(double) * N_PARTICLE * DIMENSION);
+    mas = (double *)malloc(sizeof(double) * N_PARTICLE);
+    memset(mas, 0, sizeof(double) * N_PARTICLE);
 
     for (int i = 0; i < N_PARTICLE; i++) {
-        mas[i] = rand() % N_PARTICLE;
+        mas[i] = (rand() % RAND_MAX) * MAX_MASS / RAND_MAX;
+        pos[i * DIMENSION + 0] = (double)(rand() % RAND_MAX) / (double)RAND_MAX;
+        pos[i * DIMENSION + 1] = (double)(rand() % RAND_MAX) / (double)RAND_MAX;
     }
 }
 
 void simulate() {
     for (int t = 0; t < TIME_LENGTH; t++) {
+        memset(acc, 0, sizeof(double) * N_PARTICLE * DIMENSION);
+        for (int i = 0; i < N_PARTICLE; i++) {
+            pos[(t + 1) * N_PARTICLE * DIMENSION + i * DIMENSION + 0] =
+                pos[t * N_PARTICLE * DIMENSION + i * DIMENSION + 0];
+            pos[(t + 1) * N_PARTICLE * DIMENSION + i * DIMENSION + 1] =
+                pos[t * N_PARTICLE * DIMENSION + i * DIMENSION + 1];
+        }
         for (int i = 0; i < N_PARTICLE; i++) {
             for (int j = 0; j < N_PARTICLE; j++) {
+                if (i == j) continue;
+                double dx =
+                    pos[t * N_PARTICLE * DIMENSION + i * DIMENSION + 0] -
+                    pos[t * N_PARTICLE * DIMENSION + j * DIMENSION + 0];
+                double dy =
+                    pos[t * N_PARTICLE * DIMENSION + i * DIMENSION + 1] -
+                    pos[t * N_PARTICLE * DIMENSION + j * DIMENSION + 1];
+                double r = dist(i, j);
+                acc[i * DIMENSION + 0] += -G * mas[j] * dx / (r * r * r);
+                acc[i * DIMENSION + 1] += -G * mas[j] * dy / (r * r * r);
             }
+        }
+        for (int i = 0; i < N_PARTICLE; i++) {
+            vel[i * DIMENSION + 0] += acc[i * DIMENSION + 0] * dt;
+            vel[i * DIMENSION + 1] += acc[i * DIMENSION + 1] * dt;
+        }
+        for (int i = 0; i < N_PARTICLE; i++) {
+            pos[(t + 1) * N_PARTICLE * DIMENSION + i * DIMENSION + 0] +=
+                vel[i * DIMENSION + 0] * dt;
+            pos[(t + 1) * N_PARTICLE * DIMENSION + i * DIMENSION + 1] +=
+                vel[i * DIMENSION + 1] * dt;
         }
     }
 }
